2e1f669f10e27414b9fceeb2bbbada36c669157b
S3N-CA5 <bladesnrocks@gmail.com>
2017-02-21 22:50:46 -1000

    problem with positioning

--- js/app.js
+++ js/app.js
@@ -3,17 +3,10 @@ console.log ("app.js linked!");
 function turnCard(id) {
 	console.log("clicked", id);
 	document.getElementById(id).style.transform = 'rotateY(180deg)';
-	
-
 	if (document.getElementById(id).style.transform === 'rotateY(180deg)'){
-
 		setTimeout(function() {
-
 			document.getElementById(id).style.transform = 'rotateY(0deg)';
-			
 		},1000);
-
 	}
 	console.log("end");
-
 }
